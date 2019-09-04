4cfd4c69db43ac574bf06d1280d9c681df6715f6
laobocheng <1063779828@qq.com>
2019-09-04 14:20:03 +0800

    清除兼容库注解

--- src/main/java/cc/xiaobaicz/viewgroup/RelativeRadioGroup.java
+++ src/main/java/cc/xiaobaicz/viewgroup/RelativeRadioGroup.java
@@ -4,7 +4,6 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Build;
-import android.support.annotation.IdRes;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -106,7 +105,7 @@ public class RelativeRadioGroup extends RelativeLayout {
      * @see #getCheckedRadioButtonId()
      * @see #clearCheck()
      */
-    public void check(@IdRes int id) {
+    public void check(int id) {
         // don't even bother
         if (id != -1 && (id == mCheckedId)) {
             return;
@@ -123,7 +122,7 @@ public class RelativeRadioGroup extends RelativeLayout {
         setCheckedId(id);
     }
 
-    private void setCheckedId(@IdRes int id) {
+    private void setCheckedId(int id) {
         mCheckedId = id;
         if (mOnCheckedChangeListener != null) {
             mOnCheckedChangeListener.onCheckedChanged(this, mCheckedId);
@@ -137,8 +136,7 @@ public class RelativeRadioGroup extends RelativeLayout {
         }
     }
 
-    @IdRes
-    public int getCheckedRadioButtonId() {
+        public int getCheckedRadioButtonId() {
         return mCheckedId;
     }
 
@@ -193,7 +191,7 @@ public class RelativeRadioGroup extends RelativeLayout {
          * @param group the group in which the checked radio button has changed
          * @param checkedId the unique identifier of the newly checked radio button
          */
-        public void onCheckedChanged(RelativeRadioGroup group, @IdRes int checkedId);
+        public void onCheckedChanged(RelativeRadioGroup group, int checkedId);
     }
 
     private class CheckedStateTracker implements CompoundButton.OnCheckedChangeListener {
